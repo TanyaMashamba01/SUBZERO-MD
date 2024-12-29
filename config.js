c9969f73cc67ef84ae0468c00df35e39df551365
TanyaMashamba01 <mildredtanyamashamba01@gmail.com>
2024-12-29 21:07:59 +0200

    Update config.js

--- config.js
+++ config.js
@@ -28,21 +28,21 @@ module.exports = {
 
 SESSION_ID: process.env.SESSION_ID || "ENTER YOUR SUBZERO SESSION HERE",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true", //TRUE OR FALSE
-OWNER_NUMBER: process.env.OWNER_NUMBER || "18062212660",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "263777756184",
 MODE: process.env.MODE || "public",  // PRIVATE OR PUBLIC
-AUTO_VOICE: process.env.AUTO_VOICE || "true",
+AUTO_VOICE: process.env.AUTO_VOICE || "false",
 AUTO_STICKER: process.env.AUTO_STICKER || "false",
 AUTO_REPLY: process.env.AUTO_REPLY || "false",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://i.postimg.cc/m2jtqrBC/Screenshot-20241216-090753-Video-Maker.jpg", // DO NOT CHANGE
-ALIVE_MSG: process.env.ALIVE_MSG || "sᴜʙᴢᴇʀᴏ ᴀɪ ɪs ᴀʟɪᴠᴇ. ᴋᴇᴇᴘ sᴜᴘᴘᴏʀᴛɪɴɢ *Zᴇʀᴏ Cᴏᴅᴇʀs❄️*\n\n> ᴅᴇᴠᴇʟᴏᴘᴇᴅ ʙʏ ᴍʀ ꜰʀᴀɴᴋ",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://i.top4top.io/p_3236rxa990.jpg", // DO NOT CHANGE
+ALIVE_MSG: process.env.ALIVE_MSG || "s❄️Joshuamambo1 is here for you",
 ANTI_LINK: process.env.ANTI_LINK || "true",
 ANTI_BAD: process.env.ANTI_BAD || "true",
-PREFIX: process.env.PREFIX || ".", // .,!/?
+PREFIX: process.env.PREFIX || "#", // .,!/?
 FAKE_RECORDING: process.env.FAKE_RECORDING || "false",
 AUTO_REACT: process.env.AUTO_REACT || "false",
 HEART_REACT: process.env.HEART_REACT || "false",
 OWNER_REACT: process.env.OWNER_REACT || "false",
-BOT_NAME: process.env.BOT_NAME || "『 ＳＵＢＺＥＲＯ - ＭＤ 』",
+BOT_NAME: process.env.BOT_NAME || "『 JOSHKING - ＭＤ 』",
 OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39", // omdbapi.com
 };
 
